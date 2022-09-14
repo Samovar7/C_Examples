15d2ace88a9dca078f9732e087f2fa18aa3f6483
Eugene <leugene7@gmail.com>
2022-09-14 11:44:59 +0500

    Examples003_Sum change add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-int numberA = 31;
-int numberB = 5;
+int numberA = new Random().Next(1,11); //будут генерится числа от 1 до 10 включительно
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,11);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
