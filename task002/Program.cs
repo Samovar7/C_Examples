74f8e554deb8f21344a1151b686bfc0786baf95c
Eugene <leugene7@gmail.com>
2022-09-15 23:13:01 +0500

    Семинар 1 задача 2 является ли число 1 квадратом числа 2

---
+++ task002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int user_number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int user_number2 = Convert.ToInt32(Console.ReadLine());
+if (user_number1 == user_number2 * user_number2)
+{
+Console.WriteLine($"Число {user_number1} является квадратом числа {user_number2}");
+}
+else
+{
+Console.WriteLine($"Число {user_number1} не является квадратом числа {user_number2}");
+}
