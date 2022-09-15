04d15c4d79503027f229bb866aa75b4a3dbb3239
Eugene <leugene7@gmail.com>
2022-09-15 22:53:52 +0500

    Семинар 1 задача 1 квадрат числа

---
+++ task001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите значение:");
+int user_number = Convert.ToInt32(Console.ReadLine());
+int result = user_number * user_number;
+Console.WriteLine($"Квадрат числа {user_number} равен {result}");
+
