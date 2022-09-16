53b268f95342d88399921eb55fa5a0f488c7d212
Eugene <leugene7@gmail.com>
2022-09-16 12:44:52 +0500

    Семинар 1 Домашнее задание задача 8

---
+++ task007_HomeWork-8/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+Console.WriteLine("Введите  число:");
+//int user_number = new Random().Next(1,100);
+int user_number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (user_number < count)
+{
+   while (count >= user_number)
+{
+       if (count%2 == 0)
+    {
+        Console.Write($"{count}: ");
+    }
+     count--;
+} 
+}
+else
+{
+    while (count <= user_number)
+    {
+       if (count%2 == 0)
+        {
+        Console.Write($"{count}: ");
+        }
+        count++;
+    }
+}
