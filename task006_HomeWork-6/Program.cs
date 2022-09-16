5799201176819c4972891cf3de436cadddc9419b
Eugene <leugene7@gmail.com>
2022-09-16 12:09:09 +0500

    Семинар 1 Домашнее задание задача 6

---
+++ task006_HomeWork-6/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число 
+и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+*/
+Console.WriteLine("Введите  число:");
+//int user_number1 = new Random().Next(1,100);
+int user_number1 = Convert.ToInt32(Console.ReadLine());
+
+if (user_number1%2 == 0)
+{
+    Console.WriteLine($"Вы ввели число {user_number1}, это число является Четным");
+}
+else
+{
+Console.WriteLine($"Вы ввели число {user_number1}, это число нечетное");
+}
