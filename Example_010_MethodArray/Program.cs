bab52d193c17e17b11709428eb0a6642dd1df8e7
Eugene <leugene7@gmail.com>
2022-09-17 16:50:22 +0500

    Лекция-2 Функции и Массивы

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {10,70,36,41,12,22,70,16};
+
+int n = array.Length;
+int find = 70;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {{
+        Console.WriteLine($"Искомый элемент {find} находится на {index} позиции в массиве");
+        break;
+    }}
+
+
+    index++;
+}
