012190dcbf3d811903302df9db830cac8b81716e
EugeneL <leugene7@gmail.com>
2022-09-22 17:40:53 +0500

    HomeWork for Seminar #2

---
+++ task012_HomeWork10/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число:");
+int user_number = Convert.ToInt32(Console.ReadLine());
+if (user_number < 100 || user_number > 999)
+{
+    Console.WriteLine ("Вы ввели не трехзначное число, попробуйте еще раз.");
+}
+else 
+{ int second_num = (user_number / 10)%10;
+Console.WriteLine($"Вторая цифра в числе: {second_num}");
+}
---
+++ task013_HomeWork13/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int get_number_size ( int div) // Определяем количество цифр в числе
+{
+    int number_size = 0;
+    while (div > 0)
+        {
+        div = div / 10;
+        number_size++;
+        }
+    Console.WriteLine($"В веденном числе {number_size} цифр.");
+    return number_size;
+}
+
+Console.Write("Введите любое целое число: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите какую цифру по порядку в вашем числе, нужно вывести на экран: ");
+int seq_number = Convert.ToInt32(Console.ReadLine());
+int size_number = get_number_size(user_number);
+
+if (size_number < seq_number)
+{
+    Console.WriteLine($"В вашем числе нет {seq_number} цифры.");
+}
+else
+{
+    int seq = (user_number / 10^(size_number - seq_number))%10;
+    Console.WriteLine($"{seq_number} цифра в вашем числе {user_number} равна {seq}");
+}
